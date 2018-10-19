b1d39875932faaa9388d9ff24158992576312356
hjtaki <hjtaki@gmail.com>
2018-10-19 10:59:23 -0700

    first commit

---
+++ CalorieCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieCalculator
+//
+//  Created by Alex Jintak Han on 2018-10-19.
+//  Copyright © 2018 AlexHan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
